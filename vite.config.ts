d02b0292ba581b2d22b301c8592aa95fe3cc68a6
Brandon Huber <brandon@hubermx.com>
2026-03-03 13:13:22 -0600

    Fix base url in production

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/tagnabbit/' : '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/tagnabbit/' : '/',
   // server: {
   //   proxy: {
   //     '/bst-proxy': {
