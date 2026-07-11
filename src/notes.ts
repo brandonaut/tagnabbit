324e9eaf0b1e52ac049ceea04c66a699a932698a
Brandon Huber <brandon@hubermx.com>
2026-07-10 20:26:03 -0500

    Move NOTE_FREQUENCIES into notes.ts ahead of PitchPipe merge

--- src/notes.ts
+++ src/notes.ts
@@ -8,3 +8,23 @@ export const ENHARMONIC: Record<string, string> = {
   Ab: "G#",
   Bb: "A#",
 }
+
+export const NOTE_FREQUENCIES: Record<string, number> = {
+  C: 261.63,
+  "C#": 277.18,
+  Db: 277.18,
+  D: 293.66,
+  "D#": 311.13,
+  Eb: 311.13,
+  E: 329.63,
+  F: 349.23,
+  "F#": 369.99,
+  Gb: 369.99,
+  G: 392.0,
+  "G#": 415.3,
+  Ab: 415.3,
+  A: 440.0,
+  "A#": 466.16,
+  Bb: 466.16,
+  B: 493.88,
+}
