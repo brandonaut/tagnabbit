9e7799040eeee714ff80553a61d3c23d1f6a39b5
Brandon Huber <brandon@hubermx.com>
2026-08-16 16:29:44 -0500

    Release v0.1.0

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "0.0.7"
-export const RELEASE_DATE = "2026-08-15"
+export const APP_VERSION = "0.1.0"
+export const RELEASE_DATE = "2026-08-16"
 export const RELEASE_URL = `https://github.com/brandonaut/tagnabbit/releases/tag/v${APP_VERSION}`
